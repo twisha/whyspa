0f52691f6379de6c5f7f07de647a3852fe7c3bf8
twisha <twisha.trp@gmail.com>
2014-05-16 14:57:08 -0400

    Fixed controller invoking twice issue

--- POC-WhySPA/Scripts/MusicSpa/Albums/Shared/musicAlbumsRouteConfig.js
+++ POC-WhySPA/Scripts/MusicSpa/Albums/Shared/musicAlbumsRouteConfig.js
@@ -2,13 +2,16 @@
     var routeConfig = function ($routeProvider) {
         $routeProvider.when("/", {
             controller: "musicAlbumsController",
-            templateUrl: window.musicApp.rootUrl + "MusicSpa/Albums/List"
+            templateUrl: window.musicApp.rootUrl + "MusicSpa/Albums/List",
+            reloadOnSearch: false
         }).when("/:genreId", {
             controller: "musicAlbumsController",
-            templateUrl: window.musicApp.rootUrl + "MusicSpa/Albums/List"
+            templateUrl: window.musicApp.rootUrl + "MusicSpa/Albums/List",
+            reloadOnSearch: false
         }).when("/Edit/:id", {
             controller: "musicAlbumEditController",
-            templateUrl: window.musicApp.rootUrl + "MusicSpa/Albums/Edit"
+            templateUrl: window.musicApp.rootUrl + "MusicSpa/Albums/Edit",
+            reloadOnSearch: false
         }).otherwise({ redirectTo: "/" });
     };
     routeConfig.$inject = ['$routeProvider'];
--- POC-WhySPA/Scripts/MusicSpa/MovieDvds/Shared/movieDvdsRouteConfig.js
+++ POC-WhySPA/Scripts/MusicSpa/MovieDvds/Shared/movieDvdsRouteConfig.js
@@ -2,13 +2,16 @@
     var routeConfig = function ($routeProvider) {
         $routeProvider.when("/", {
             controller: "movieDvdsController",
-            templateUrl: window.musicApp.rootUrl + "MusicSpa/MovieDvds/List"
+            templateUrl: window.musicApp.rootUrl + "MusicSpa/MovieDvds/List",
+            reloadOnSearch: false
         }).when("/:genreId", {
             controller: "movieDvdsController",
-            templateUrl: window.musicApp.rootUrl + "MusicSpa/MovieDvds/List"
+            templateUrl: window.musicApp.rootUrl + "MusicSpa/MovieDvds/List",
+            reloadOnSearch: false
         }).when("/Edit/:id", {
             controller: "movieDvdEditController",
-            templateUrl: window.musicApp.rootUrl + "MusicSpa/MovieDvds/Edit"
+            templateUrl: window.musicApp.rootUrl + "MusicSpa/MovieDvds/Edit",
+            reloadOnSearch: false
         });
     };
     routeConfig.$inject = ['$routeProvider'];
